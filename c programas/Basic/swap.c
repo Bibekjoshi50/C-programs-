c7f4f758a63148a168e09402d09bca6df9a562d9
Bibek Joshi <bebikjoshi118@gmail.com>
2024-12-16 20:16:26 +0545

    swap

--- c programas/Basic/swap.c
+++ c programas/Basic/swap.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
 int main(){
     int a=10,b=20,c;
-printf
+printf("Before swapping a=%d and b=%d\n",a,b);
+c=a;
+a=b;
+b=c;
+printf("After swapping a=%d and b=%d\n",a,b);
+return 0;
 }
