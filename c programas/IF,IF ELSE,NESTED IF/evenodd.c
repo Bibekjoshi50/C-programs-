72f0d82ede177fa4dacd00e4e1eb4e3cfbf368e3
Bibek Joshi <bebikjoshi118@gmail.com>
2024-12-24 20:31:01 +0545

    even odd

---
+++ c programas/IF,IF ELSE,NESTED IF/evenodd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter a number\n");
+    scanf("%d",&n);
+    if (n/2==0)
+    {
+       orintf("Even Number ");
+    }
+    else{
+        printf("odd number");
+    }
+    return 0;
+}
