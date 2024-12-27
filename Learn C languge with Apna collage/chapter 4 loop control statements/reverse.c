16cb7bdbc4c72ed5ecd14c4a0acb83d97d4a76a7
Bibek Joshi <bebikjoshi118@gmail.com>
2024-12-27 22:14:52 +0545

    loop problems

---
+++ Learn C languge with Apna collage/chapter 4 loop control statements/reverse.c
@@ -0,0 +1,13 @@
+#include<stdint.h>
+int main(){
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    
+    for (int i = 10; i>=1; i--)
+    {
+        printf("%d\n",i);
+    }
+    return 0;
+    
+}
