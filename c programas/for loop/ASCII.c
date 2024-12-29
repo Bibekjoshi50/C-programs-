1f47006a9bbd94b1d95acde4d7873a2bc0c4aace
Bibek Joshi <bebikjoshi118@gmail.com>
2024-12-29 20:45:44 +0545

    c programs

---
+++ c programas/for loop/ASCII.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    for ( char ch ='A'; ch<='Z';ch++)
+    {
+       printf("%c = %d\n",ch,ch);
+    }
+    return 0;
+}
---
+++ c programas/for loop/even.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    printf ("Even number b/w 1 and 20 is given below\n");
+    for (int  i = 2; i<= 20; i=i+2)
+    {
+        printf("%d ",i);
+    }
+    return 0;
+}  
