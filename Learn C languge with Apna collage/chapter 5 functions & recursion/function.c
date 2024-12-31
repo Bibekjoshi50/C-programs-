92cb13544bf15b20bdf32c0d5bf9298b92398cdb
Bibek Joshi <bebikjoshi118@gmail.com>
2024-12-31 20:24:18 +0545

    c

--- Learn C languge with Apna collage/chapter 5 functions & recursion/function.c
+++ Learn C languge with Apna collage/chapter 5 functions & recursion/function.c
@@ -10,5 +10,6 @@ int main (){
     return 0;
 }
 void printHello(){ // Function definition
-    printf("Hello !\n");
+    printf("Hello My name is Bibek Joshi  !\n");
+    printf("I am currently studing Bsc CSIT at Tribhuwan University\n ");
 }
---
+++ Learn C languge with Apna collage/chapter 5 functions & recursion/twofunction.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void printHello();
+void printGoodbye();
+
+int main(){
+    printGoodbye();
+    printHello();
+    return 0;
+
+}
+void printHello(){
+    printf("Hello !\n");
+
+}
+void printGoodbye(){
+    printf("Good Bye!!!\n");
+}
