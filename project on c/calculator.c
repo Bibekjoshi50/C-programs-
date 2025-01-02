ca386c789693f3c5c76088a30da964f15534a706
Bibek Joshi <bebikjoshi118@gmail.com>
2025-01-02 22:12:47 +0545

    calcultor

---
+++ project on c/calculator.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main(){
+double num1, num2;
+char operator;
+printf("Enter first number: ");
+scanf("%lf",&num1);
+
+printf("Enter an operator  (+,-,*,/):");
+scanf("%c",&operator);
+
+printf("Enter second number : ");
+scanf("%lf",&num2);
+
+switch (operator)
+{
+case '+':
+printf("Result: %.2lf\n",num1 + num2");
+    break;
+
+    case '-':
+    printf("Result : %2lf\n",num1 - num2);
+    break;
+
+    case '*':
+    printf("Result: %.2lf\n",num1*num2);
+    break;
+
+    case '/':
+    printf("Result: %2lf\n",num1,num2);
+    break;
+
+
+
+
+default:
+printf("Invalid operator ! please enter  +,-,*,/\n");
+    break;
+}
+return 0;
+}
