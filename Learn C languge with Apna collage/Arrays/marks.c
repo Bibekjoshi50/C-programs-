2c0e257dea5a89f1bc6080b2ba9a0aa6ac75314d
Bibek Joshi <bebikjoshi118@gmail.com>
2025-01-05 02:27:07 +0545

    array

---
+++ Learn C languge with Apna collage/Arrays/marks.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+
+    int marks[3];
+
+    printf("Enter the marks of physics: ");
+    scanf("%d",&marks[0]);
+
+        printf("Enter the marks of chemistry: ");
+    scanf("%d",&marks[1]);
+
+
+        printf("Enter the marks of mathematics: ");
+    scanf("%d",&marks[2]);
+
+
+    printf(" The marks of physics = %d, chemistry = %d, mathematicsff f  = %d", marks[0],marks[1],marks[2]);
+
+
+return 0;
+}
