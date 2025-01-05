f1107c158fc58fb9f2e48ef3d52d112b6cf1d292
Bibek Joshi <bebikjoshi118@gmail.com>
2025-01-05 14:12:05 +0545

    2 d array

--- Learn C languge with Apna collage/Arrays/2darray.c
+++ Learn C languge with Apna collage/Arrays/2darray.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 
-void storeTable(int arr[] [], int n, int m, int number);
+void storeTable(int arr[] [10], int n, int m, int number);
 
 int main(){
 int tables[2][10];
@@ -10,10 +10,19 @@ storeTable(tables, 0, 10, 2);
 return 0;
 }
 
-void storeTable( int arr[] [], int n, int m, int number){
+void storeTable( int arr[] [10], int n, int m, int number){
 
 for (int i=0; i<m; i++){
 
     arr[n] [m] = number *(i+1);
 }
+
+
+printf("\n");
+  for ( int  i = 0; i < 10; i++)
+  {
+    printf("%d\t", tables[1][i]);
+  }
+  
+
 }
