4abaa457bf740ca06c89c4d3b32851c8cd287e93
Bibek Joshi <bebikjoshi118@gmail.com>
2025-01-18 10:24:50 +0545

    new c

---
+++ programs old/simpleinterest.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int p,n;
+    float r,si;
+    printf("Enter vales of prncipal(p),time(n), rate(r) : ");
+    scanf("%d  %d  %f",&p,&n,&r);
+    si =p * n * r /100;
+    printf("The simple interest is : %f\n",si);
+    return 0;
+}
