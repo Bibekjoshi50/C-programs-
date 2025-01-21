bade23b3cf75bb2f59f7dbae09b7aa920d82dd8e
Bibek Joshi <bebikjoshi118@gmail.com>
2025-01-21 20:21:59 +0545

    c

---
+++ C Complete Course with prashant sir/chapter 3/instruction.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+
+int age ;
+int i=10, j=25;
+float temperature =98.6;
+float a=1.5, b=1.99; +2.3 *1.44;
+float p= 5, q=p *6;
+char name[50];
+int x,y,z =5;
+ 
+
+// printf("Enter your age : ");
+    return 0;
+
+
+}
