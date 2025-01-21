3057de6133f6f19d999f511b932545abd59629e8
Bibek Joshi <bebikjoshi118@gmail.com>
2025-01-21 20:39:42 +0545

    chapter 3

---
+++ C Complete Course with prashant sir/chapter 3/alpha.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int ad;
+    float kot,delta, alpha, beta, gamma;
+    ad =3200;
+    kot = 0.044;
+    delta = alpha *beta/gamma+3.2 *2/5;
+}
---
+++ C Complete Course with prashant sir/chapter 3/type-conversion.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b =4;
+    float x=9.5;
+    double y= 23.4;
+    long int z= 400;
+    double d;
+    a= z/b+b*x-y;
+    printf("integer  : %d\n",a);
+    return 0;
+}
