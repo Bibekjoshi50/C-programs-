23371d330cdb1b23073f095b381a6c8ba4a78835
Bibek Joshi <bebikjoshi118@gmail.com>
2025-01-22 22:30:09 +0545

    new c program

---
+++ C Complete Course with prashant sir/2 varible data type/Programs/welcome.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char name[20];
+    printf("plese enter your name : ");
+    scanf("19s",name);
+    printf("welcome %s to bibek joshi coding ",name);
+    return 0;
+}
