395cc242a0c506991419f06a8ac13cb49085a993
Bibek Joshi <bebikjoshi118@gmail.com>
2025-01-22 12:58:16 +0545

    new

---
+++ C Complete Course with prashant sir/2 varible data type/Programs/leftstar.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    printf("right pyrimid ");
+    printf("*\n");
+     printf("**\n");
+      printf("***\n");
+       printf("****\n");
+        printf("*****\n");
+}
