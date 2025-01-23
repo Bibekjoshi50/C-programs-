c20d9ac9d7f39a3619394e78be5adb7d83a27bb7
Bibek Joshi <bebikjoshi118@gmail.com>
2025-01-23 16:58:49 +0545

    size of int

--- C Complete Course with prashant sir/2 varible data type/Programs/data type .c
+++ C Complete Course with prashant sir/2 varible data type/Programs/data type .c
@@ -1,4 +1,11 @@
 #include<stdio.h>
 int main(){
-	
+	int integer;
+	float decimal;
+	double doub;
+	char character;
+	 printf("\nThe size of int is %d bytes",sizeof(integer));
+	 printf("\nThe size of float  is %d bytes",sizeof(decimal));
+	 printf("\nThe size of double is %d bytes",sizeof(doub));
+	 printf("\nThe size of char is %d bytes",sizeof(character));
 }
