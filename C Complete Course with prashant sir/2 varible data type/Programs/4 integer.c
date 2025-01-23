be04fcd7c4836ab84bd06d701736942692f9484e
Bibek Joshi <bebikjoshi118@gmail.com>
2025-01-23 16:50:07 +0545

    two number

---
+++ C Complete Course with prashant sir/2 varible data type/Programs/4 integer.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int first,second;
+	printf("Please enter the first number : ");
+	scanf("%d",&first);
+	
+	printf("Please enter the second number : ");
+	scanf("%d",&second);
+	
+	printf("The to numbers you entered are %d and %d  ", first , second );
+	return 0;
+	
+}
