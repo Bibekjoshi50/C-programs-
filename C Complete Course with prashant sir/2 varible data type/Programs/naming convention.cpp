8cfc3a81ea77f0271a11b2d747761c94d2ed6596
Bibek Joshi <bebikjoshi118@gmail.com>
2025-01-23 17:19:59 +0545

    c program

---
+++ C Complete Course with prashant sir/2 varible data type/Programs/naming convention.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	char first_name[20] = "Bibek ";
+	char last_name[20] = "Nepal";
+	int age =20;
+	
+	printf("first name :%s , last name : %s , Age : %d", first_name,last_name ,age);
+
+}
---
+++ C Complete Course with prashant sir/2 varible data type/Programs/square.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+float side,area;
+printf("Please enter the side of square : ");
+scanf("%f",&side);
+
+area = side * side;
+printf("The area of your square is %f ", area );
+
+
+return 0;
+	
+}
