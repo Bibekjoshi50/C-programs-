43ef4c8caf36a4c2a4342ff99e1e9236d9a57e7c
Bibek Joshi <bebikjoshi118@gmail.com>
2025-01-29 20:43:41 +0545

    swap

---
+++ C Complete Course with prashant sir/2 varible data type/Programs/swap.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	
+	int a,b ,c;
+	 printf("Please enter the first number :");
+	 scanf("%d ",&a);
+	 
+	 printf("Now enter the second number : ");
+	 scanf("%d",&b);
+	
+	
+	c=a;
+	a=b;
+	b=c;
+	return 0;
+	
+}
