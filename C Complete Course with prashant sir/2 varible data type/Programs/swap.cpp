2cfd9e37495849b390d1ebc39dac07a6dbd677f6
Bibek Joshi <bebikjoshi118@gmail.com>
2025-01-30 00:14:46 +0545

    swap

--- C Complete Course with prashant sir/2 varible data type/Programs/swap.cpp
+++ C Complete Course with prashant sir/2 varible data type/Programs/swap.cpp
@@ -2,16 +2,18 @@
 int main(){
 	
 	int a,b ,c;
-	 printf("Please enter the first number :");
+	 printf("Please enter the first number: ");
 	 scanf("%d ",&a);
 	 
-	 printf("Now enter the second number : ");
+	 printf("Now ,enter the second number: ");
 	 scanf("%d",&b);
 	
 	
-	c=a;
-	a=b;
-	b=c;
+	c = a;
+	a = b;
+	b = c;
+	
+	printf("The value of fist value  is %d  and second is %d", a ,b );
 	return 0;
 	
 }
