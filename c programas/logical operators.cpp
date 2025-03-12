fcd40220378e3eed58df9208e39a5e2df3e45c6c
Bibek Joshi <bebikjoshi118@gmail.com>
2025-03-12 17:56:13 +0545

    relational \operator

---
+++ c programas/logical operators.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int a=10 , b=50, c=30;
+	
+	if (a>b&&b>c)
+	printf("a is gretest");
+	if(b>a&&b>c)
+	printf("b is gretest");
+	if(c>a&&c>b)
+	printf("c is greatest");
+}
+
