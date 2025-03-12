e5122b231ede981034db4d750cd90cccad9e61b1
Bibek Joshi <bebikjoshi118@gmail.com>
2025-03-12 16:30:00 +0545

    arithmetic operator

---
+++ c programas/Arithmetic operator example.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	int x= 16 , y=3;
+	int add ,sub, multi,div,mod;
+	add = x+y;
+	sub = x-y;
+	multi=x*y;
+	div =x/y;
+	mod =x%y;
+	
+	
+	printf("Addition =%d\n",add);
+	printf("Subtraction =%d\n",sub);
+	printf("Multiplication =%d\n",multi);
+	printf("Division =%d\n",div);
+	printf("MOdulus =%d\n",mod);
+}
