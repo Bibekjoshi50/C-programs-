e1160b65b915d6ff684cbf571c78116b3d05753e
Bibek Joshi <bebikjoshi118@gmail.com>
2025-03-13 08:39:42 +0545

    switch

---
+++ c programas/switch statement.cpp
@@ -0,0 +1,27 @@
+
+#include<stdio.h>
+int main(){
+	int p =2;
+	switch(p)
+	{
+		case 1:
+			printf("It is case 1");
+			break;
+			
+			case 2 :
+			printf("It is case 2");
+			break;
+			
+			case 3:
+			printf("It is case 3");
+			break;
+			
+			case 4:
+			printf("It is case 4");
+			break;
+			
+			default:
+				printf("no case matched");
+	}
+	return 0;
+}
