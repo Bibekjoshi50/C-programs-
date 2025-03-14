1849564dc24200f1d8bc83eab9e2508cdec6bd14
Bibek Joshi <bebikjoshi118@gmail.com>
2025-03-14 10:30:13 +0545

    address

---
+++ c programas/address in c.cpp
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int *p;
+	printf("value of a =%d\n",a);
+	printf("address of a =%d\n",&a);
+	printf("value of p = %d\n",p);
+	printf("address of p =%d\n",&p);
+	printf("value of *p=%d\n",*p);
+	return 0;
+}
